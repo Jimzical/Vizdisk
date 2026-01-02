797cad65d64c90c167f15d9f1ce3e138360fd44b
Jimzical <arishkumar2003@gmail.com>
2026-01-02 18:50:37 +0530

    Add gzip compression support for JSON responses in HandleData

--- internal/disktree/server.go
+++ internal/disktree/server.go
@@ -1,9 +1,11 @@
 package disktree
 
 import (
+	"compress/gzip"
 	"embed"
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 //go:embed index.html
@@ -18,6 +20,15 @@ func HandleIndex(w http.ResponseWriter, r *http.Request) {
 func HandleData(rootNode *D3Node) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			w.Header().Set("Content-Encoding", "gzip")
+			gz := gzip.NewWriter(w)
+			defer gz.Close()
+			json.NewEncoder(gz).Encode(rootNode)
+			return
+		}
+
 		json.NewEncoder(w).Encode(rootNode)
 	}
 }
